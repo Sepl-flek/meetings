f4f40ebbc681f5b75bc3d1b49e6e0d8c690fbb88
Sepl-flek <154527662+Sepl-flek@users.noreply.github.com>
2025-06-07 21:03:51 +0300

    add Feedback model

--- meetings/user/admin.py
+++ meetings/user/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from user.models import Profile, FeedBack
+
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(FeedBack)
--- meetings/user/models.py
+++ meetings/user/models.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
-class Clients(models.Model):
+class Profile(models.Model):
     STATUS_CHOICE = (
         ('beginner', 'Новичок'),
         ('active', 'Активный'),
@@ -12,6 +15,30 @@ class Clients(models.Model):
         ('legend', 'Легенда'),
     )
 
-    user = models.OneToOneField(User, on_delete=models.PROTECT)
-    information = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    information = models.CharField(max_length=100, blank=True, default='')
     status = models.CharField(max_length=15, choices=STATUS_CHOICE, default='beginner')
+    feedbacks = models.ManyToManyField('self', through_fields=('user', 'author'),
+                                       symmetrical=False, through='FeedBack')
+
+    def __str__(self):
+        return f"Профиль {self.user.username}"
+
+
+class FeedBack(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='authored_feedbacks')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='feedbacks_received')
+    feedback = models.CharField(max_length=255, null=True)
+    rating = models.PositiveIntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+
+    def __str__(self):
+        return f"Отзыв от {self.author} для {self.user}"
+
+    class Meta:
+        unique_together = ('author', 'user')
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
